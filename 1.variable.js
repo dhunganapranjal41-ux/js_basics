ec6bdda642500c0cae9023d249b5de971e40f7c3
pranjaldhungana <dhunganapranjal41@gmail.com>
2026-06-16 07:35:44 +0545

    add: variable

---
+++ 1.variable.js
@@ -0,0 +1,38 @@
+
+console.log("Hello world");
+
+//! variables
+// console.log(10);
+
+//! variable declaration methods
+//* let, const , var
+
+// int a = 12
+//  a = 'dsg'
+var x;
+x = 34;
+var a = 10; //
+a = "hello";
+var a = 345;
+
+console.log(a);
+// console.log(a); //
+
+let b = 100;
+b = 45;
+
+// let b = 566;
+console.log(b);
+
+const c = 123;
+// const c = 45;
+var d;
+let e;
+
+const f = 45;
+console.log(b + a);
+let sum = b + a;
+
+console.log(sum);
+
+// c = 45;
